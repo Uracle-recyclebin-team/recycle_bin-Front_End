06ad0c49876cbf1ea27831df82054dd031bbbca9
Deahun-k <kimdh8315@naver.com>
2023-05-16 15:36:53 +0900

    Front-End-04
    04

--- map/my-app/src/building.jsx
+++ map/my-app/src/building.jsx
@@ -1,7 +1,70 @@
+import React, { useState } from 'react';
+import logo from './image/logo_w.png';
+import image from './image/noimage-w-900.png';
+import image2 from './image/map.png';
+import { Link } from 'react-router-dom';
+import './main.jsx';
+import './main.css';
+import './building.css';
+
 function Building() {
+    const [isHidden, setIsHidden] = useState(false);
+    const toggleDiv = () => {
+        setIsHidden(!isHidden);
+    };
     return (
-        <div>
-            <h1>asfg</h1>
+        <div className="Building">
+            <header className="Main-header">
+                <div className='Main-Name'>
+                    <img src={logo} className="Main-logo" alt="logo" />
+                    <p>경일대 안내앱</p>
+                </div>
+                <ul className="Main-Top">
+                    <li><Link to="/" style={{ textDecoration: "none", color: "white" }}>지도</Link></li>
+                    <li><Link to="/" style={{ textDecoration: "none", color: "white" }}>내선번호</Link></li>
+                    <li><Link to="/phone" style={{ textDecoration: "none", color: "white" }}>학과 교수님</Link></li>
+                    <li><Link to="/" style={{ textDecoration: "none", color: "white" }}>사무실</Link></li>
+                </ul>
+            </header>
+            <h1>6 호관</h1>
+            <div className="Building-Middle" style={{ display: isHidden ? 'none' : 'block' }}>
+                <img src={image} className="Building-img" alt="Building-img" />
+                <div className='Building-Middle-Btn'>
+                    <button onClick={toggleDiv}>
+                        <img src="https://cdn1.iconfinder.com/data/icons/bootstrap-vol-2/16/chevron-double-up-40.png" alt="BtnImg" />
+                    </button>
+                </div>
+            </div>
+            <div className='Building-Middle2' style={{ display: isHidden ? 'block' : 'none' }}>
+                <img src={image} className="Building-img" alt="Building-img" />
+                <img src={image2} className="Building-img2" alt="Building-img2" />
+                <button className='leftBtn'>
+                    <img src="https://cdn1.iconfinder.com/data/icons/bootstrap-vol-2/16/chevron-left-40.png" alt="LeftBtnImg" />
+                </button>
+                <button className='rightBtn'>
+                    <img src="https://cdn1.iconfinder.com/data/icons/bootstrap-vol-2/16/chevron-right-40.png" alt="RightBtnImg" />
+                </button>
+            </div>
+            <div className="Building-Bottom">
+                <table className='Building-Bottom-Table'>
+                    <tr>
+                        <td>1층</td>
+                        <td>ㄱㄱ과</td>
+                    </tr>
+                    <tr>
+                        <td>2층</td>
+                        <td>ㄴㄴ과</td>
+                    </tr>
+                    <tr>
+                        <td>3층</td>
+                        <td>ㄷㄷ과</td>
+                    </tr>
+                    <tr>
+                        <td>4층</td>
+                        <td>ㄹㄹ과</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     );
 }
--- map/my-app/src/main.jsx
+++ map/my-app/src/main.jsx
@@ -14,9 +14,9 @@ function Main() {
         </div>
         <ul className="Main-Top">
           <li><Link to="/" style={{ textDecoration: "none", color: "white" }}>지도</Link></li>
-          <li><Link to="/building" style={{ textDecoration: "none", color: "white" }}>내선번호</Link></li>
+          <li><Link to="/" style={{ textDecoration: "none", color: "white" }}>내선번호</Link></li>
           <li><Link to="/phone" style={{ textDecoration: "none", color: "white" }}>학과 교수님</Link></li>
-          <li><Link to="/building" style={{ textDecoration: "none", color: "white" }}>사무실</Link></li>
+          <li><Link to="/" style={{ textDecoration: "none", color: "white" }}>사무실</Link></li>
         </ul>
       </header>
       <div className="Map_Middle">
@@ -24,36 +24,36 @@ function Main() {
       </div>
       <div className="Map_Bottom">
         <ul>
-          <li>01 본관</li>
-          <li>02 중앙도서관/학생서비스센터</li>
-          <li>03 3호관(교수연수동)</li>
-          <li>05 5호관</li>
-          <li>06 6호관</li>
-          <li>07 7호관</li>
-          <li>08 8호관</li>
-          <li>09 9호관</li>
-          <li>10 학생생활관(지성관)</li>
-          <li>11 11호관</li>
-          <li>12 창의적융합교육센터</li>
-          <li>13 학생회관/학생식당/복지매장</li>
-          <li>14 14호관/KIU철도아카데미</li>
-          <li>17 17호관/BLS센터</li>
-          <li>18 18호관</li>
-          <li>19 학생생활관(웅비관)</li>
-          <li>20 KIU피트니스 센터</li>
-          <li>21 종힙체육관</li>
-          <li>22 22호관/국제교류교육원</li>
-          <li>23 R&DB센터/창업지원단</li>
-          <li>24 학생생활관(목련관)</li>
-          <li>25 산학교육관</li>
-          <li>26 강당/코워킹 스페이스</li>
-          <li>27 풋살경기장</li>
-          <li>28 자율주행차융합기술연구소</li>
-          <li>30 대운동장</li>
-          <li>31 노천강당</li>
-          <li>32 취업처/대학일자리센터/현장실습지원센터/학생상담센터</li>
-          <li>33 학생생활관(일청관)</li>
-          <li>36 하늘정원</li>
+          <li value="1">01 본관</li>
+          <li value="2">02 중앙도서관/학생서비스센터</li>
+          <li value="3">03 3호관(교수연수동)</li>
+          <li value="5">05 5호관</li>
+          <li value="6"><Link to="/building" style={{ textDecoration: "none", color: "black" }}>06 6호관</Link></li>
+          <li value="7">07 7호관</li>
+          <li value="8">08 8호관</li>
+          <li value="9">09 9호관</li>
+          <li value="10">10 학생생활관(지성관)</li>
+          <li value="11">11 11호관</li>
+          <li value="12">12 창의적융합교육센터</li>
+          <li value="13">13 학생회관/학생식당/복지매장</li>
+          <li value="14">14 14호관/KIU철도아카데미</li>
+          <li value="17">17 17호관/BLS센터</li>
+          <li value="18">18 18호관</li>
+          <li value="19">19 학생생활관(웅비관)</li>
+          <li value="20">20 KIU피트니스 센터</li>
+          <li value="21">21 종힙체육관</li>
+          <li value="22">22 22호관/국제교류교육원</li>
+          <li value="23">23 R&DB센터/창업지원단</li>
+          <li value="24">24 학생생활관(목련관)</li>
+          <li value="25">25 산학교육관</li>
+          <li value="26">26 강당/코워킹 스페이스</li>
+          <li value="27">27 풋살경기장</li>
+          <li value="28">28 자율주행차융합기술연구소</li>
+          <li value="30">30 대운동장</li>
+          <li value="31">31 노천강당</li>
+          <li value="32">32 취업처/대학일자리센터/현장실습지원센터/학생상담센터</li>
+          <li value="33">33 학생생활관(일청관)</li>
+          <li value="36">36 하늘정원</li>
         </ul>
       </div>
     </div>
